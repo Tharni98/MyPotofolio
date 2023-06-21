b07e4058db15bcd51d779caf17c1dd393e2b7c80
Tharani Harshika <tharaniharshika1@gmail.com>
2023-06-21 15:54:01 +0530

    loader new update

--- assets/javaScript/loaderJS.js
+++ assets/javaScript/loaderJS.js
@@ -8,12 +8,12 @@ window.addEventListener("load", function () {
 });
 
 
-// in addition to that if you want to run a code as soon as the DOM created
-document.addEventListener("DOMContentLoaded", function () {
-    // you can write anything you want here, and it will run just after creating the DOM object
-    let h1 = document.createElement("h1");
-    h1.textContent = "HI there";
-
-    let body=document.querySelector("body");
-    body.prepend(h1);
-});
+// // in addition to that if you want to run a code as soon as the DOM created
+// document.addEventListener("DOMContentLoaded", function () {
+//     // you can write anything you want here, and it will run just after creating the DOM object
+//     let h1 = document.createElement("h1");
+//     h1.textContent = "HI there";
+//
+//     let body=document.querySelector("body");
+//     body.prepend(h1);
+// });
