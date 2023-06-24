a2f6780999757d9423f53938a22849ce51f67990
Tharani Harshika <tharaniharshika1@gmail.com>
2023-06-24 22:02:59 +0530

    CSS update

--- assets/javaScript/myImageJS.js
+++ assets/javaScript/myImageJS.js
@@ -1,8 +1,8 @@
 document.getElementById("profileImg").animate(
     [
         // keyframes
-        { transform: "translateX(0px)" },
-        { transform: "translateX(-200px)" },
+        // { transform: "translateX(0px)" },
+        { transform: "translateX(200px)" },
         { transform: "translateX(0px)" },
         { transform: "translateX('paused')" },
 
--- assets/javaScript/welcomeJS.js
+++ assets/javaScript/welcomeJS.js
@@ -7,3 +7,22 @@ function scrollFunction() {
         document.getElementById("hi").style.fontSize = "90px";
     }
 }
+
+
+document.getElementById("welcome").animate(
+    [
+        // keyframes
+        // { transform: "translateX(0px)" },
+        { transform: "translateX(-200px)" },
+        { transform: "translateX(0px)" },
+        { transform: "translateX('paused')" },
+
+    ],
+    {
+
+        // element.style.animationPlayState = 'paused';
+        // timing options
+        duration: 1200,
+        iterations:1,
+    }
+);
